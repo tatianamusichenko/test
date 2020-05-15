7659146a4aec4563e2458a3ea89cb9ee83b2b3e0
IGORIA JINKA <tatiana.020801@gmail.com>
2020-05-15 20:26:39 +0300

    userdata

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,34 @@
 import '../styles/index.scss';
 
-const a = 8;
-console.log("Const = ", a);
-const b = 4;
-console.log("Const = ", b);
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
+let user = {
+    name: "Katya",
+    surname: "Vasulchuk",
+    age: "30",
+    data: "12.05.1990",
+    height: "177.5",
+    hasAnimal: true,
+    getname: function () {
+        console.log("name", this.name);
+    },
+    getsurname: function () {
+        console.log("surname", this.surname);
+    },
+    getdata: function () {
+        console.log("data", this.data);
+    }
+};
+console.log(user.name);
+console.log(user.surname);
+console.log(user.age);
+console.log(user.data);
+console.log(user.height);
+console.log(user.hasAnimal);
+
+let food = ["pizza", "rolls", "roast potatoes", "chicken wings", "chocolate cake"]
+console.log(food);
+
+user.getname();
+user.getsurname();
+user.getdata();
+
+console.log(user.name + " " + user.surname + " " + user.data);
